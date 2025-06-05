8c178801c1effb9629c102b671ec16975d96146b
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2025-06-05 19:39:10 +0800

    feat: practicing pixel replacements for dynamic character ideas.

--- main.lua
+++ main.lua
@@ -1,46 +1,28 @@
-local playerSprite = love.image.newImageData('/res/icon.png')
-local player = love.graphics.newImage(playerSprite)
-local windowWidth = love.graphics.getWidth()
-local windowHeight = love.graphics.getHeight()
+local function normalizeColor(r, g, b, a)
+	return {r/HEX, g/HEX, b/HEX, a/HEX}
+end
 
 function love.load()
-    print(("Width: %d\nHeight: %d"):format(windowWidth, windowHeight))
+	HEX = 255
+	Cube = love.graphics.newImage('/res/icon.png')
+	Cube:setFilter("nearest", "nearest")
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("c") then
-        local rectangle = love.image.newImageData(32, 32)
-        local t = table
-        t:insert(0)
-        t:insert(0)
-        t:insert(1)
-        t:insert(1)
-        for x = 0, 31, 1 do
-            for y = 0, 31, 1 do
-                rectangle:setPixel(x, y, t)
-            end
-        end
-        player:replacePixels(rectangle)
-        print(dt)
-    end
-    if love.keyboard.isDown('w') then
-        love.graphics.clear()
-        love.graphics.draw(player, love.graphics.transformPoint(0, 1))   
-    end
-    if love.keyboard.isDown('a') then
-        love.graphics.clear()
-        love.graphics.draw(player, love.graphics.transformPoint(-1, 0))   
-    end
-    if love.keyboard.isDown('s') then
-        love.graphics.clear()
-        love.graphics.draw(player, love.graphics.transformPoint(0, -1))   
-    end
-    if love.keyboard.isDown('d') then
-        love.graphics.clear()
-        love.graphics.draw(player, love.graphics.transformPoint(1, 0))   
-    end
+	
 end
 
 function love.draw()
-    love.graphics.draw(player)
+	love.graphics.draw(Cube, 0, 0, nil, 16, 16)
+end
+
+function love.keypressed(key)
+	if key == 'd' then
+		local imageData = love.image.newImageData(2, 2, "rgba8", '0000000000000000')
+		imageData:setPixel(0, 0, unpack(normalizeColor(250, 0, 20, 255)))
+		imageData:setPixel(1, 0, unpack(normalizeColor(0, 250, 20, 255)))
+		imageData:setPixel(0, 1, unpack(normalizeColor(0, 20, 250, 255)))
+		imageData:setPixel(1, 1, unpack(normalizeColor(0, 100, 250, 255)))
+		Cube:replacePixels(imageData)
+	end
 end
